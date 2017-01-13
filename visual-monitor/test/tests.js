e03b4dcd4e47bfef77eeafb7715140802c76c208
mccrodp <info@opensourceopenmind.ninja>
2017-01-13 15:41:35 +0000

    Replaced X-Team.com address with Digitial Ocean IP.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -41,7 +41,7 @@ var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
 var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
 var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
 
-var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'https://x-team.com';
+var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://138.197.88.198';
 
 var resultsCallback = process.env.DEBUG ? console.log : shoovWebdrivercss.processResults;
 
